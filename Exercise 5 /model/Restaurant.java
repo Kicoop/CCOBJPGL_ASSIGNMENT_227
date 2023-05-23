77e5a5d9189863095219590f927c5640cfc82021
Francis Celada <119907392+Kicoop@users.noreply.github.com>
2023-05-23 12:03:47 +0800

    Add files via upload

---
+++ Exercise 5 /model/Restaurant.java
@@ -0,0 +1,23 @@
+package model;
+
+class Restaurant {
+
+    private String color;
+    private String food;
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFood() {
+        return this.food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+}
---
+++ Exercise 5 /model/Zarks.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Zarks extends Restaurant {
+
+}
